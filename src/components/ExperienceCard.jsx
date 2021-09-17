d6a5d7941ce866764167bc0ff4d7d6e60d893bb1
Mohsen Raeisi <mrt1373@gmail.com>
2021-09-17 15:43:14 +0200

    Update ExperienceCard.jsx

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -30,7 +30,7 @@ const ExperienceCard = ({data}) => {
     return ( 
         
         <Fade left duration={1000} distance="40px">
-            <Card style={{flex: 1}} className="shadow-lg--hover shadow border-0 text-center rounded">
+            <Card style={{flex: 1}} className="shadow-lg--hover shadow border-0 text-center rounded mb-5">
                 <CardHeader style={{background: rgb(colorArrays)}} >
                     <h5 className="text-white">{data.company}</h5>
                 </CardHeader>
@@ -38,8 +38,9 @@ const ExperienceCard = ({data}) => {
                     <img ref={imgRef} className=" bg-white rounded-circle mb-3 img-center img-fluid shadow-lg " src={data.companylogo} style={{ width: "100px" }} onLoad={() => getColorArrays()} alt=""/>
                     <CardTitle tag="h5">{data.role}</CardTitle>
                     <CardSubtitle>{data.date}</CardSubtitle>
+                    <CardSubtitle>{data.desc}</CardSubtitle>
+
                     <CardText className="description my-3 text-left">
-                        {data.desc}
                         <ul>
                             {
                                 data.descBullets ? 

3ebedd52bd61fe0045e1ba97b0ae9ae8e6630de2
Mohsen Raeisi <mrt1373@gmail.com>
2021-09-29 12:33:52 +0200

    Update FeedbackCard.jsx

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -12,7 +12,14 @@ const FeedbackCard = ({ data }) => {
 						<div className="pl-4">
 							<h5 className="text-info">Témoignage <a href={data.url}>{data.from}</a></h5>
 							<p className="description mt-3 font-italic"> « {data.feedback} »</p>
-							<h6 >   <img src={data.img} style={{width: "50px"}} alt="" className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"/> {data.name} </h6>
+							<div className="row"> 
+								<div className="col-3">
+								<img src={data.img} style={{width: "50px"}} alt="" className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"/>
+								</div>
+								<div className="col p-2">
+									<h6>{data.name} </h6>
+								</div>
+							</div>
 
 						</div>
 					</div>
